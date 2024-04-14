05b11bf8d7677a14c88088bd3267cc13b9513dcb
Leo Zhang <leozhang0214@gmail.com>
2024-04-14 22:33:30 +0100

    Parse JSON array, number
    Array can be empty and follows standard. Number parsing follows the standard too. Both tested. Works solidly (tested reasonably, may still be some minor bugs but works in general).  A few minor changes to the API too, such as wrapping all values in unique pointer for safer memory. Next up: string parsing (the torture part thanks to UTF8/16 rules).

--- src/json.cpp
+++ src/json.cpp
@@ -1,4 +1,5 @@
 // JSON parser implementation as per https://datatracker.ietf.org/doc/html/rfc8259
+#include <cmath>
 #include "json.h"
 
 
@@ -11,66 +12,234 @@ ValueType Value::type() const {
     return this->_type;
 }
 
-void* const Value::value() const {
+const void* const Value::value() const {
     return this->_value;
 }
 
-
-Value parse_array(const std::string& string, int& i) {
-    i++;
-}
-
-
-Value parse_object(const std::string& string, int& i) {
-    i++;
-}
-
-
-Value parse_number(const std::string& string, int& i) {
-    
-}
-
-
-Value parse_string(const std::string& string, int& i) {
-    i++;
-}
-
-
-Value parse_literal_name(const std::string& string, int& i) {
-    std::string literal_name = "";
-    for (; i < string.size(); ++i) {
-        literal_name += string[i];
-        if (literal_name.size() > 5 ) {
-            throw;
-        }
-        if (LITERAL_NAME_MAP.count(literal_name)) {
-            i++;
-            return Value(ValueType::literal_name, LITERAL_NAME_MAP[literal_name]);
-        }
-    }
+const ValuePtr& Value::operator[](std::size_t pos) const {
+    // By default, cannot use array index. Erroneous to do so.
     throw;
 }
 
 
-Value parse_json(const std::string& string) {
-    Value result;
-    bool empty = true;
+const ValuePtr& Array::operator[](std::size_t pos) const {
+    // Arrays can logically have elements accessed by index.
+    return ((ValueArray*)(this->value()))->operator[](pos);
+}
+
+
+ValuePtr parse_array(const std::string& string, int& i) {
+    i++; // Opening square bracket is known.
+    ValueArray* array = new ValueArray;
+    bool expecting_comma = false;
+    for (; i < string.size(); ++i) {
+        char c = string[i];
+        if (WHITESPACE.count(c)) {
+            continue;
+        }
+        if (expecting_comma) {
+            if (!STRUCTURAL_CHARS.count(c)) {
+                throw;
+            }
+            switch (STRUCTURAL_CHARS[c]) {
+                case Structural::value_separator:
+                    // Comma as expected.
+                    expecting_comma = false;
+                    continue;
+                case Structural::end_array:
+                    // Alternatively, array can end here too.
+                    return std::make_unique<Array>(ValueType::array, array);
+                default:
+                    throw;
+            }
+        }
+        if (STRUCTURAL_CHARS.count(c)) {
+            // Allow opening of array, object, or closing or array
+            // Only allow comma if expected (not expecting a value).
+            switch (STRUCTURAL_CHARS[c]) {
+                case Structural::begin_array:
+                    array->push_back(parse_array(string, i));
+                    break;
+                case Structural::begin_object:
+                    array->push_back(parse_object(string, i));
+                    break;
+                case Structural::end_array:
+                    // Just had a comma.
+                    // Must not end on comma, but array can be empty.
+                    if (!array->empty()) {
+                        throw;
+                    }
+                    return std::make_unique<Array>(ValueType::array, array);
+                default:
+                    // Not expecting any other structural character at this time.
+                    throw;
+            }
+        } else if (c == STRING_QUOTES) {
+            array->push_back(parse_string(string, i));
+        } else if (c == MINUS_SIGN || isdigit(c)) {
+            array->push_back(parse_number(string, i));
+        } else {
+            // Can only be a literal, or complete nonsense otherwise.
+            array->push_back(parse_literal_name(string, i));
+        }
+        expecting_comma = true;
+    }
+    // End of string reached without array closure.
+    throw;
+}
+
+
+ValuePtr parse_object(const std::string& string, int& i) {
+    i++;
+}
+
+
+ValuePtr parse_number(const std::string& string, int& i) {
+    // Optional minus sign.
+    bool negative = string[i] == MINUS_SIGN;
+    if (negative) {
+        i++;
+    }
+    double integer_part = 0;
+    double fractional_part = 0;
+    int fractional_digits = 0;
+    double exponent = 0;
+    enum ParsingPart {Integer, Fraction, Exponent};
+    ParsingPart parsing_part = Integer;
+    bool leading_zero = false;
+    bool integer_part_empty = true;
+    bool decimal_point_seen = false;
+    bool exponent_seen = false;
+    bool exponent_empty = true;
+    bool exponent_negative = false;
+    for (; i < string.size(); ++i) {
+        char c = string[i];
+        switch (parsing_part) {
+            case Integer:
+                // Possible chars: ., 0-9 (0 not first), e, E
+                if (c == DECIMAL_POINT) {
+                    decimal_point_seen = true;
+                    parsing_part = Fraction;
+                } else if (isdigit(c)) {
+                    if (c == '0' && integer_part_empty) {
+                        // (Possibly) Insignificant leading 0.
+                        leading_zero = true;
+                    }
+                    if (leading_zero && !integer_part_empty) {
+                        // Insignificant leading 0 is illegal.
+                        throw;
+                    }
+                    integer_part = integer_part * 10 + (c - '0');
+                    integer_part_empty = false;
+                } else if (tolower(c) == EXPONENT) {
+                    // Early exponent without fractional part.
+                    exponent_seen = true;
+                    parsing_part = Exponent;
+                } else {
+                    // Unrecognised character - assume end of number.
+                    goto end_of_number;
+                }
+                break;
+            case Fraction:
+                // Possible chars: 0-9, e, E
+                if (isdigit(c)) {
+                    fractional_digits++;
+                    // Nth digit after decimal point is power of 10^-n.
+                    fractional_part += (c - '0') * std::pow(10, -fractional_digits);
+                } else if (tolower(c) == EXPONENT) {
+                    exponent_seen = true;
+                    parsing_part = Exponent;
+                } else {
+                    goto end_of_number;
+                }
+                break;
+            case Exponent:
+                // Possible chars: + (first only), - (first only), 0-9
+                if (exponent_empty && c == PLUS_SIGN) {
+                    // Positive exponent, no effect since it is the default.
+                    continue;
+                } else if (exponent_empty && c == MINUS_SIGN) {
+                    exponent_negative = true;
+                } else if (isdigit(c)) {
+                    exponent = exponent * 10 + (c - '0');
+                    exponent_empty = false;
+                } else {
+                    goto end_of_number;
+                }
+                break;
+        }
+    }
+    end_of_number:
+    // Decrement so that the current unknown char can be handled in parent scope.
+    i--;
+    // Integer part must not be empty.
+    // Decimal point must be followed by one or more digits.
+    // If 'e' is seen, it must be followed by one or more digits.
+    if (
+        integer_part_empty
+        || (decimal_point_seen && fractional_digits == 0)
+        || (exponent_seen && exponent_empty)
+    ) {
+        throw;
+    }
+    if (exponent_negative) {
+        exponent = -exponent;
+    }
+    double* number = new double((integer_part + fractional_part) * std::pow(10, exponent));
+    if (negative) {
+        *number = -*number;
+    }
+    return std::make_unique<Value>(ValueType::number, number);
+}
+
+
+ValuePtr parse_string(const std::string& string, int& i) {
+    i++; // Opening double quote.
+}
+
+
+ValuePtr parse_literal_name(const std::string& string, int& i) {
+    std::string literal_name = "";
+    for (; i < string.size(); ++i) {
+        literal_name += string[i];
+        if (literal_name.size() > 5) {
+            // false is the longest literal, if still going, invalid...
+            throw;
+        }
+        if (LITERAL_NAMES.count(literal_name)) {
+            bool* result = nullptr;
+            switch (LITERAL_NAMES[literal_name]) {
+                case LiteralName::true_:
+                    result = new bool(true);
+                    break;
+                case LiteralName::false_:
+                    result = new bool(false);
+                    break;
+            }
+            return std::make_unique<Value>(ValueType::literal_name, result);
+        }
+    }
+    // End of string reached without finding a literal.
+    throw;
+}
+
+
+ValuePtr parse_json(const std::string& string) {
+    ValuePtr result = nullptr;
     for (int i = 0; i < string.size(); ++i) {
         char c = string[i];
         if (WHITESPACE.count(c)) {
             // Ignore insignificant whitespace.
             continue;
         }
-        if (!empty) {
+        if (result != nullptr) {
             // Cannot have second part of the toplevel data... error
             throw;
         }
-        empty = false;
         if (STRUCTURAL_CHARS.count(c)) {
-            Structural structural_char = STRUCTURAL_CHARS[c];
             // In this context, either array/object opening is expected.
             // Anything else is erroneous.
-            switch (structural_char) {
+            switch (STRUCTURAL_CHARS[c]) {
                 case Structural::begin_array:
                     result = parse_array(string, i);
                     break;
@@ -91,7 +260,8 @@ Value parse_json(const std::string& string) {
             result = parse_literal_name(string, i);
         }
     }
-    if (empty) {
+    if (result == nullptr) {
+        // Nothing found but whitespace.
         throw;
     }
     return result;
--- src/json.h
+++ src/json.h
@@ -1,8 +1,11 @@
 // Corresponding header file for the JSON parser implementation.
 #pragma once
+#include <cstdint>
 #include <map>
+#include <memory>
 #include <set>
 #include <string>
+#include <vector>
 
 
 // Structural characters in JSON.
@@ -29,44 +32,66 @@ enum class ValueType {object, array, number, string, literal_name};
 // Strings open and close with DOUBLE quotes.
 const char STRING_QUOTES = '"';
 
-// Numbers may start with a minus sign.
+// Numbers/exponents may start with a minus sign.
 const char MINUS_SIGN = '-';
+// Exponents may start with a plus sign.
+const char PLUS_SIGN = '+';
+// Numbers may contain a decimal point to separate the integer/fractional part.
+const char DECIMAL_POINT = '.';
+// Numbers may contain an exponent part (scientific notation).
+const char EXPONENT = 'e';
 
-// Literal name to actual value (pointer).
-static std::map<std::string, void*> LITERAL_NAME_MAP = {
-    {"true", new bool {true}},
-    {"false", new bool {false}},
-    {"null", nullptr}
+// Valid literal names.
+enum class LiteralName {true_, false_, null};
+static std::map<std::string, LiteralName> LITERAL_NAMES = {
+    {"true", LiteralName::true_},
+    {"false", LiteralName::false_},
+    {"null", LiteralName::null}
 };
 
+
+class Value;
+// Wrap a dynamic JSON value in a memory-safe unique pointer.
+typedef std::unique_ptr<Value> ValuePtr;
 // Represents a JSON value. Will be polymorphic such that the type of
 // the value can be deduced at runtime, not compile-time.
 class Value {
     private:
         ValueType _type;
-        void* _value;
+        const void* _value;
     public:
         Value() = default;
         Value(ValueType, void*);
         ValueType type() const;
-        void* const value() const;
+        const void* const value() const;
+        virtual const ValuePtr& operator[](std::size_t) const;
 };
+
+// Represents a JSON array, storing an ordered sequence of
+// heterogeneous values (not a traditional array!).
+class Array : public Value {
+    using Value::Value;
+    const ValuePtr& operator[](std::size_t) const override;
+};
+
+// The internal representation of a JSON array of values.
+typedef std::vector<ValuePtr> ValueArray;
 
 // Parses a string object representing the JSON data.
 // Will raise an exception if the JSON data is invalid.
-Value parse_json(const std::string&);
+ValuePtr parse_json(const std::string&);
 
 // Parses a JSON array.
-Value parse_array(const std::string&, int&);
+ValuePtr parse_array(const std::string&, int&);
 
 // Parses a JSON object.
-Value parse_object(const std::string&, int&);
+ValuePtr parse_object(const std::string&, int&);
 
 // Parses a JSON number.
-Value parse_number(const std::string&, int&);
+ValuePtr parse_number(const std::string&, int&);
 
 // Parses a JSON string literal (not the JSON string itself).
-Value parse_string(const std::string&, int&);
+ValuePtr parse_string(const std::string&, int&);
 
 // Parses a JSON literal name (true, false or null).
-Value parse_literal_name(const std::string&, int&);
+ValuePtr parse_literal_name(const std::string&, int&);
